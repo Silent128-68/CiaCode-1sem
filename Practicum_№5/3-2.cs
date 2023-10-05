f30acbc7c1df9d7a20cde7c6fc07d081aa35ae39
Kuzya <116298294+Silent128-68@users.noreply.github.com>
2023-10-05 21:26:23 +0400

    Update 3-2.cs

--- Practicum_№5/3-2.cs
+++ Practicum_№5/3-2.cs
@@ -4,7 +4,7 @@ class Program
 {
     static double[] cache; // кэш для хранения уже вычисленных значений
 
-    static double GetNthTerm(int n)
+    static double GetN(int n)
     {
         if (n== 1)
         {
@@ -16,22 +16,22 @@ class Program
             return cache[n];
         }
 
-        double previousTerm = GetNthTerm(n - 1);
-        double currentTerm = previousTerm / (Math.Pow(n, 2) + n + 1);
+        double previous = GetN(n - 1);
+        double current = previous / (Math.Pow(n, 2) + n + 1);
         
-        cache[n] = currentTerm; // сохраняем вычисленное значение в кэше
+        cache[n] = current; // сохраняем вычисленное значение в кэше
         
-        return currentTerm;
+        return current;
     }
 
-    static void Main()
+    static void Main() 
     {
         Console.Write("Введите число n: ");
         int n = int.Parse(Console.ReadLine());
         
         cache = new double[n + 1]; // инициализируем кэш с размером n + 1
         
-        double nthTerm = GetNthTerm(n);
+        double nthTerm = GetN(n);
         
         Console.WriteLine($"Значение {n}-го члена последовательности: {nthTerm}");
     }
