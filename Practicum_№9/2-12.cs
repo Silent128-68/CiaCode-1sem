ab9da7184a529358473b645c25bedfacf9f925db
Kuzya <116298294+Silent128-68@users.noreply.github.com>
2023-11-22 23:51:07 +0400

    Update 2-12.cs

--- Practicum_№9/2-12.cs
+++ Practicum_№9/2-12.cs
@@ -6,57 +6,102 @@ class Program
     // Метод для сравнения компонент файлов попарно
     static bool Compare(string file1Path, string file2Path)
     {
+        // Проверка на существование файлов
+        if (!File.Exists(file1Path) || !File.Exists(file2Path))
+        {
+            return false;
+        }
+
         // Открываем файлы с помощью StreamReader
         using (StreamReader reader1 = new StreamReader(file1Path))
         using (StreamReader reader2 = new StreamReader(file2Path))
         {
-            int char1;
-            int char2;
+            string line1;
+            string line2;
+            bool filesMatch = true;
 
-            // Считываем символы из обоих файлов пока не достигнем конца файла
-            while ((char1 = reader1.Read()) != -1 && (char2 = reader2.Read()) != -1)
+            // Сравниваем строки поочередно
+            while ((line1 = reader1.ReadLine()) != null && (line2 = reader2.ReadLine()) != null)
             {
-                // Если символы не совпадают, возвращаем false
-                if (char1 != char2)
+                if (line1 != line2)
                 {
-                    return false;
+                    // Выводим различия в строках, если они не совпадают
+                    Console.WriteLine($"Различие в строках:\nФайл 1: {line1}\nФайл 2: {line2}");
+                    filesMatch = false;
                 }
             }
 
-            // Если все символы совпадают, возвращаем true
-            return true;
+            // Выводим сообщение, если строки совпадают
+            if (filesMatch)
+            {
+                Console.WriteLine("Строки файлов попарно совпадают.");
+            }
+
+            return filesMatch;
         }
     }
-    
-    // Метод для нахождения индекса первой компоненты, в которой файлы отличаются
-    static int FindFirstDifference(string file1Path, string file2Path)
+
+    // Метод для вывода позиций несовпадающих символов
+    static void PrintDifferencePositions(string line1, string line2, int lineIndex)
     {
+        Console.Write($"Различие в символах строки {lineIndex + 1}: ");
+        for (int i = 0; i < Math.Min(line1.Length, line2.Length); i++)
+        {
+            if (line1[i] != line2[i])
+            {
+                Console.Write($"{i + 1} ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    // Метод для нахождения различий между файлами
+    static void FindDifferences(string file1Path, string file2Path)
+    {
+        // Проверка на существование файлов
+        if (!File.Exists(file1Path) || !File.Exists(file2Path))
+        {
+            Console.WriteLine("Один или оба файла не существуют.");
+            return;
+        }
+
         // Открываем файлы с помощью StreamReader
         using (StreamReader reader1 = new StreamReader(file1Path))
         using (StreamReader reader2 = new StreamReader(file2Path))
         {
-            int char1;
-            int char2;
-            int index = 0;
+            string line1;
+            string line2;
+            int lineIndex = 0;
 
-            // Считываем символы из обоих файлов пока не достигнем конца файла
-            while ((char1 = reader1.Read()) != -1 && (char2 = reader2.Read()) != -1)
+            // Сравниваем строки поочередно
+            while ((line1 = reader1.ReadLine()) != null)
             {
-                // Если символы не совпадают, возвращаем текущий индекс
-                if (char1 != char2)
+                line2 = reader2.ReadLine();
+
+                // Если один файл короче другого, выводим сообщение и прекращаем сравнение
+                if (line2 == null)
                 {
-                    return index;
+                    Console.WriteLine($"Файл 2 короче файла 1. Различие начинается с строки {lineIndex + 1}");
+                    break;
                 }
 
-                // Увеличиваем индекс для перехода к следующей компоненте
-                index++;
+                // Выводим различия в символах, если строки не совпадают
+                if (line1 != line2)
+                {
+                    PrintDifferencePositions(line1, line2, lineIndex);
+                }
+
+                lineIndex++;
             }
 
-            // Если все символы совпадают, возвращаем текущий индекс
-            return index;
+            // Если файл 2 имеет больше строк, чем файл 1, выводим дополнительные строки
+            while ((line2 = reader2.ReadLine()) != null)
+            {
+                Console.WriteLine($"Файл 1 короче файла 2. Дополнительная строка в файле 2: {line2}");
+            }
         }
     }
-    
+
     static void Main()
     {
         Console.WriteLine("Введите путь к первому файлу:");
@@ -65,15 +110,6 @@ class Program
         Console.WriteLine("Введите путь ко второму файлу:");
         string file2Path = Console.ReadLine();
 
-        bool filesMatch = Compare(file1Path, file2Path);
-
-        if (filesMatch)
-        {
-            Console.WriteLine("Компоненты файлов попарно совпадают.");
-        }
-        else
-        {
-            Console.WriteLine("Файлы отличаются в компоненте с индексом: " + FindFirstDifference(file1Path, file2Path));
-        }
+        FindDifferences(file1Path, file2Path);
     }
 }
