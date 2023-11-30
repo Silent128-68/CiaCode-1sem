aafc3eaf0cb7c49f3cd9149c907e348ee47a96cc
Kuzya <116298294+Silent128-68@users.noreply.github.com>
2023-12-01 00:35:54 +0400

    Update 4-12.cs

--- Practicum_№7/4-12.cs
+++ Practicum_№7/4-12.cs
@@ -5,41 +5,47 @@ class Program
     static void Main()
     {
         Console.Write("Введите размерность массива: ");
-        int n = int.Parse(Console.ReadLine()); 
+        int n;
+        
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("Пожалуйста, введите положительное целое число.");
+        }
 
-        int[,] array = new int[n, n]; // создаем двумерный массив размером n x n
+        int[,] array = new int[n, n];
 
         Console.WriteLine("Введите элементы массива:");
 
         for (int i = 0; i < n; i++)
         {
-            string[] input = Console.ReadLine().Split(); // считываем строку с элементами массива
+            string[] input = Console.ReadLine().Split();
 
             for (int j = 0; j < n; j++)
             {
-                array[i, j] = int.Parse(input[j]); // преобразуем и записываем элементы в массив
+                array[i, j] = int.Parse(input[j]);
             }
         }
 
-        int[] result = new int[n]; // массив для хранения номеров последних нечетных элементов
+        int[] result = new int[n];
 
         for (int j = 0; j < n; j++)
         {
-            int lastnumber = -1; 
+            int lastnumber = -1;
 
-            for (int i = 0; i < n; i++)
+            for (int i = n - 1; i >= 0; i--)
             {
-                if (array[i, j] % 2 != 0) 
+                if (array[i, j] % 2 != 0)
                 {
-                    lastnumber = i; // обновляем номер последнего нечетного элемента
+                    lastnumber = i;
+                    break; // прерываем цикл после нахождения первого нечетного элемента
                 }
             }
 
-            result[j] = lastnumber; // записываем номер последнего нечетного элемента в новый массив
+            result[j] = lastnumber;
         }
 
-        // Выводим результат
         Console.WriteLine("Номера последних нечетных элементов в каждом столбце:");
+
         for (int j = 0; j < n; j++)
         {
             Console.WriteLine($"Столбец {j + 1}: {result[j]}");
